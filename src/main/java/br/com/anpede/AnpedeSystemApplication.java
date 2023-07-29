7203cb0b17bc957a8711cba7d9abcc0315087e81
marcosrogerio-jrf <marcosrogerio.jrf@gmail.com>
2023-07-29 11:39:47 -0400

    Criando o projeto base

---
+++ src/main/java/br/com/anpede/AnpedeSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.anpede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnpedeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnpedeSystemApplication.class, args);
+	}
+
+}
